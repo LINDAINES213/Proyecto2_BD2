d72389e096016b3d0f8afcb026c17b78258db36a
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-04-14 02:04:21 -0600

    crud almacen

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from routes import personal
 from routes import proveedor
 from routes import producto
 from routes import cliente
+from routes import almacen
 
 app = FastAPI()
 
@@ -23,6 +24,7 @@ app.include_router(personal.personal)
 app.include_router(proveedor.proveedor)
 app.include_router(producto.producto)
 app.include_router(cliente.cliente)
+app.include_router(almacen.almacen)
 
 @app.get("/")
 def default():
