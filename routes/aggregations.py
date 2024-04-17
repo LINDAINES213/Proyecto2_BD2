c4480ab28456bd5a489626bf77dcaade13ea2da9
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-04-17 11:55:27 -0600

    agregacion vehiculos por modelo

--- routes/aggregations.py
+++ routes/aggregations.py
@@ -19,3 +19,26 @@ def sumatoria_total_ordenes():
     total_ordenes = result.single()["total_ordenes"]
 
     return {"total_ordenes": total_ordenes}
+
+@aggregations.get("/vehiculos_por_modelo_anio")
+def vehiculos_por_modelo_anio():
+    driver_neo4j = connection()
+    session = driver_neo4j.session()
+
+    query = '''
+    MATCH (v:Vehiculo)
+    RETURN v.modelo AS modelo, COUNT(v) AS cantidad
+    ORDER BY cantidad DESC
+    '''
+
+    result = session.run(query)
+
+    vehiculos_por_modelo_anio = []
+    for record in result:
+        vehiculos_por_modelo_anio.append({
+            "modelo": record["modelo"],
+            "cantidad": record["cantidad"]
+        })
+
+    return {"vehiculos_por_modelo_anio": vehiculos_por_modelo_anio}
+
