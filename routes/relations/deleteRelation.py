0aefe65900f3ac606872d89a1b7dec4bfda435a8
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-04-17 03:34:32 -0600

    delete many relations

--- routes/relations/deleteRelation.py
+++ routes/relations/deleteRelation.py
@@ -45,27 +45,23 @@ def delete_relations_bi():
 
     return {"response": "First five relations with specified label deleted successfully"}
 
-@delete_relation.delete("/delete_tiene_relations")
-def delete_relations_tiene():
+@delete_relation.delete("/delete_tiene_relations/{proveedor_id}")
+def delete_tiene_relations(proveedor_id: str):
     driver_neo4j = connection()
     session = driver_neo4j.session()
     
-    query = '''
-    MATCH ()-[r:TIENE]->()
-    RETURN r
-    LIMIT 5
-    '''
+    query = """
+    MATCH (p:Proveedor)-[r:TIENE]->()
+    WHERE p.id = $proveedor_id
+    DELETE r
+    """
 
-    result = session.run(query)
+    # Ejecuta la consulta Cypher
+    result = session.run(query, proveedor_id=proveedor_id)
 
-    for record in result:
-        relation_id = record["r"].id
-        delete_query = f'''
-        MATCH ()-[r]->()
-        WHERE ID(r) = {relation_id}
-        DELETE r
-        '''
-        session.run(delete_query)
-
-    return {"response": "First five relations with specified label deleted successfully"}
+    # Verifica si se eliminaron relaciones
+    if result.consume().counters.relationships_deleted > 0:
+        return {"message": f"Se eliminaron todas las relaciones TIENE del proveedor con ID {proveedor_id}"}
+    else:
+        return {"message": f"No se encontraron relaciones TIENE para el proveedor con ID {proveedor_id}"}
 
