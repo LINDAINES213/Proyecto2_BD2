460b8ee3d40627aad99f918d0a16c8b6c99ae43a
Diego Hernández <dahernandezsilvestre@gmail.com>
2024-04-19 22:17:24 -0600

    readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Proyecto2_BD2
+
+## Proyecto de Cadena de Suministros
+
+Este es el proyecto 2 del curso de Bases de Datos 2, que se enfoca en la implementación de un sistema de cadena de suministros utilizando varias tecnologías.
+
+### Tecnologías Utilizadas:
+
+- **Base de Datos**: Neo4j
+- **Frontend**: React
+- **Backend**: FastAPI
+- **Plataforma de Implementación**: Koyeb
+- **Base de Datos en la Nube**: Aura
+- **Automatización**: Scripts de Python para la generación de datos
+
+### Funcionamiento del Proyecto:
+
+El proyecto se centra en el seguimiento y gestión de la cadena de suministros utilizando una base de datos de grafo como Neo4j para representar las relaciones entre los diversos elementos de la cadena de suministros. El frontend desarrollado en React proporciona una interfaz intuitiva para que los usuarios visualicen y manipulen los datos de la cadena de suministros.
+
+El backend, implementado en FastAPI, se encarga de procesar las solicitudes del frontend y realizar operaciones en la base de datos Neo4j según sea necesario. La plataforma de implementación Koyeb se utiliza para alojar y escalar la aplicación en la nube.
+
+Además, se utiliza Aura como base de datos en la nube para almacenar de forma segura los datos generados por el sistema. Estos datos se generan mediante scripts de Python que simulan escenarios de la cadena de suministros para probar la robustez y eficacia del sistema.
+
+### Ver Funcionamiento:
+
+El funcionamiento completo del proyecto se muestra en [este enlace](https://www.youtube.com/watch?v=9Ymc3WK9pHk).
--- public/src/pages/Estadisticas/Estadisticas.jsx
+++ public/src/pages/Estadisticas/Estadisticas.jsx
@@ -1,14 +1,39 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { crud, leftAligned, scrollableTable,
-  centeredDiv,
+import { crud, leftAligned, editButton, scrollableTable, listaFlotante, inputContainer,
+  formGrid, centeredDiv,
  } from './Estadisticas.module.css'
 import { Loading } from '../../components';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
- const Estadisticas = () => {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const Estadisticas = () => {
+
+  const [isListVisible, setIsListVisible] = useState(false);
   const [estadistica, setEstadistica] = useState([])
   const [selectedOption, setSelectedOption] = useState('ordenDeCompra')
   const [loading, setLoading] = useState(false)
+  const [productos, setproductos] = useState([]);
+  const [productosList, setProductosList] = useState([]);
+
+
+  const toggleProductSelection = (productId) => {
+    if (productos.includes(productId)) {
+      setproductos(productos.filter(id => id !== productId));
+    } else {
+      setproductos([...productos, productId]);
+    }
+  };
+
 
   useEffect(() => {
     setLoading(true)
@@ -21,6 +46,13 @@ import { Loading } from '../../components';
         console.error('Error fetching data:', error)
       }).finally(() => {
         setLoading(false)
+        axios.get('https://frail-maryanne-uvg.koyeb.app/nodes/Producto')
+        .then(response => {
+          setProductosList(response.data.response);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error)
+        })
       })
   }, [])
 
@@ -71,7 +103,199 @@ import { Loading } from '../../components';
     }
   }
   
+  const handleConsultar = (id) => {
+    axios.get('https://frail-maryanne-uvg.koyeb.app/get_similares/{}')
+        .then(response => {
+          console.log("vehiculos", response)
+          setEstadistica(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error)
+        }).finally(() => {
+          setLoading(false)
+        })
+  }
+  const ProveedorChart = ({ estadistica }) => {
+    if (!estadistica || !estadistica.productos_proveedores || estadistica.productos_proveedores.length === 0) {
+      return <p>No hay datos disponibles para mostrar.</p>;
+    }
+  
+    const data = {
+      labels: estadistica.productos_proveedores.map(item => item.nombre),
+      datasets: [
+        {
+          label: 'Cantidad de Productos Distintos',
+          data: estadistica.productos_proveedores.map(item => item.cantidad_productos),
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+        }
+      ],
+    };
+  
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: 'white', // Cambiar el color de los ticks de la escala y
+          },
+          grid: {
+            color: 'rgba(255, 255, 255, 0.1)' // Cambiar el color de las líneas de la cuadrícula
+          }
+        },
+        x: {
+          ticks: {
+            color: 'white', // Cambiar el color de los ticks de la escala x
+          },
+          grid: {
+            color: 'rgba(255, 255, 255, 0.1)' // Cambiar el color de las líneas de la cuadrícula
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: 'white' // Cambiar el color del texto de la leyenda
+          }
+        },
+        title: {
+          display: true,
+          text: 'Cantidad de Productos por Proveedor',
+          color: 'white' // Cambiar el color del título
+        },
+        tooltip: {
+          titleColor: 'white', // Cambiar el color del título del tooltip
+          bodyColor: 'white', // Cambiar el color del cuerpo del tooltip
+          backgroundColor: 'rgba(0, 0, 0, 0.7)' // Cambiar el color de fondo del tooltip
+        }
+      }
+    };
+  
+    return <Bar data={data} options={options} />;
+  }
 
+  const ModeloVehiculoChart = ({ estadistica }) => {
+    if (!estadistica || !estadistica.vehiculos_por_modelo_anio || estadistica.vehiculos_por_modelo_anio.length === 0) {
+      return <p>No hay datos disponibles para mostrar.</p>;
+    }
+  
+    const data = {
+      labels: estadistica.vehiculos_por_modelo_anio.map(item => item.modelo),
+      datasets: [
+        {
+          label: 'Cantidad Total de Vehículos',
+          data: estadistica.vehiculos_por_modelo_anio.map(item => item.cantidad),
+          backgroundColor: 'rgba(75, 192, 192, 0.8)', // Un verde suave para el fondo
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+        }
+      ],
+    };
+  
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: 'white'
+          },
+          grid: {
+            color: 'rgba(255, 255, 255, 0.1)'
+          }
+        },
+        x: {
+          ticks: {
+            color: 'white'
+          },
+          grid: {
+            color: 'rgba(255, 255, 255, 0.1)'
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: 'white'
+          }
+        },
+        title: {
+          display: true,
+          text: 'Cantidad Total de Vehículos por Modelo',
+          color: 'white'
+        },
+        tooltip: {
+          titleColor: 'white',
+          bodyColor: 'white',
+          backgroundColor: 'rgba(0, 0, 0, 0.7)'
+        }
+      }
+    };
+  
+    return <Bar data={data} options={options} />;
+  }
+
+  
+  const TotalVentasChart = ({ estadistica }) => {
+    if (!estadistica || estadistica.total_ordenes === undefined) {
+      return <p>No hay datos disponibles para mostrar.</p>;
+    }
+  
+    const data = {
+      labels: ['Total de Ventas'],
+      datasets: [
+        {
+          label: 'Sumatoria del Total de Ventas',
+          data: [estadistica.total_ordenes],
+          backgroundColor: 'rgba(255, 159, 64, 0.2)',
+          borderColor: 'rgba(255, 159, 64, 1)',
+          borderWidth: 1,
+        }
+      ],
+    };
+  
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: 'white'
+          },
+          grid: {
+            color: 'rgba(0, 0, 0, 0.1)'
+          }
+        },
+        x: {
+          ticks: {
+            color: 'white'
+          },
+          grid: {
+            color: 'rgba(0, 0, 0, 0.1)'
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          labels: {
+            color: 'white'
+          }
+        },
+        title: {
+          display: true,
+          text: 'Sumatoria del Total de Ventas',
+          color: 'white'
+        },
+        tooltip: {
+          titleColor: 'white',
+          bodyColor: 'white',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)'
+        }
+      }
+    };
+  
+    return <Bar data={data} options={options} />;
+  }
+  
 
   const renderTable = () => {
     if (loading) {
@@ -84,67 +308,48 @@ import { Loading } from '../../components';
     switch (selectedOption) {
       case 'ordenDeCompra':
         return (
-          <div>     
-            <div className={scrollableTable}>
-              <table className='table'>
-                <thead>
-                  <th>Descripcion</th>
-                  <th>Sumatoria del total de ventas</th>
-                </thead>
-                <tbody>
-                  <tr >
-                    <td className={leftAligned}>Resultado de sumar el total de todas las ordenes de compra.</td>
-                    <td>{estadistica.total_ordenes}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+          <TotalVentasChart estadistica={estadistica} />
         );
       case 'vehiculos':
         return (
-          <div className={scrollableTable}>
-          <table className='table'>
-            <thead>
-              <th>Modelo del vehiculo</th>
-              <th>Cantidad total de vehiculos</th>
-            </thead>
-            <tbody>
-              {console.log(estadistica.vehiculos_por_modelo_anio)}
-              {estadistica && estadistica.vehiculos_por_modelo_anio && estadistica.vehiculos_por_modelo_anio.length > 0 &&
-                estadistica.vehiculos_por_modelo_anio.map((res, index) => (
-                  <tr key={index}>
-                    <td >{res.modelo}</td>
-                    <td>{res.cantidad}</td>
-                  </tr>
-                ))
-              }
-            </tbody>
-          </table>
-        </div>
+          <ModeloVehiculoChart estadistica={estadistica} />
         );
       case 'productosPorProveedor':
         return (
-          <div className={scrollableTable}>
-          <table className='table'>
-            <thead>
-              <th>Proveedor</th>
-              <th>Cantidad productos distintos</th>
-            </thead>
-            <tbody>
-              {estadistica && estadistica.productos_proveedores && estadistica.productos_proveedores.length > 0 &&
-                estadistica.productos_proveedores.map((res, index) => (
-                  <tr key={index}>
-                    <td >{res.nombre}</td>
-                    <td>{res.cantidad_productos}</td>
-                  </tr>
-                ))
-              }
-            </tbody>
-          </table>
-        </div>
+          <ProveedorChart estadistica={estadistica} />
         );
-        
+      case 'algoritmo':
+        return (
+          <div>
+            <div className={formGrid}>
+              <div className={inputContainer}>
+                  <button  type="button" style={{marginBottom: '-2vh', width:"20vw", marginLeft: "1.5vw", padding: "0.4vh", backgroundColor: "white", color: "black"}} onClick={() => setIsListVisible(!isListVisible)}>
+                    {isListVisible ? 'Ocultar opciones' : 'Seleccionar productos'}
+                  </button>
+                  {isListVisible && (
+                    <div className={listaFlotante}>
+                      {productosList.map((producto, index) => (
+                        <button
+                          key={index}
+                          type="button"
+                          onClick={() => toggleProductSelection(producto.id)}
+                          className={`btn ${productos.includes(producto.id) ? 'btn-success' : 'btn-secondary'}`}
+                          style={{backgroundColor:'white'}}
+                        >
+                          ID: {producto.id} - {producto.nombre}
+                          {productos.includes(producto.id) ? ' (Seleccionado)' : ''}
+                        </button>
+                      ))}
+                    </div>
+                  )}
+                  <button  type="button" style={{marginBottom: '-2vh', width:"20vw", marginLeft: "1.5vw", padding: "0.4vh", backgroundColor: "white", color: "black"}} onClick={() => setIsListVisible(!isListVisible)}>
+                    {isListVisible ? 'Ocultar opciones' : 'Seleccionar productos'}
+                  </button>
+                </div>
+                
+            </div>
+          </div>
+        );
       default:
         return null
     }
